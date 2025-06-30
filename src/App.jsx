a2aad283c8f4b098854e425170c7d067d808c8cb
Jaswanth-9876 <jaswanthsathuluri@gmail.com>
2025-07-01 00:01:51 +0530

    Force add all files including PlaceOrder.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import {useState} from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from "./components/Navbar/Navbar"
-import Cart from './screens/Cart/Cart'
-import Home from './screens/Home/Home'
-import PlaceOrder from './screens/PlaceOrder/PlaceOrder'
-import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
+import Cart from './screens/Cart/Cart';
+import Home from './screens/Home/Home';
+import PlaceOrder from './screens/PlaceOrder/PlaceOrder';
+import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-
-      const [showLogin,setShowLogin]=useState(false);
-
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin ? <LoginPopup setShowLogin={setShowLogin}/>:<></> }
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
 
-    <div className="app">
-      <Navbar showLogin={showLogin}  setShowLogin={setShowLogin} />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/order' element={<PlaceOrder />} />
-      </Routes>
-    </div>
+      <div className="app">
+        <Navbar showLogin={showLogin} setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
 
-    <Footer/>
-      
+      <Footer />
+
+      {/* ✅ ToastContainer added globally */}
+      <ToastContainer position="top-center" autoClose={3000} />
     </>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
